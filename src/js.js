57f330c659d20f26ab07ebc06f1aaaa11cef1e92
BeFreeI <nepogodin_p@mail.ru>
2020-05-17 21:00:31 +0300

    fix: fix eslint problems

--- src/js.js
+++
@@ -1,8 +0,0 @@
-function getMovingSum(arr) {
-    return arr.map((el, i, array) => {
-      if (i === 0) return el;
-      return el + array[i - 1];
-    });
-  }
-
-  console.log(getMovingSum([1,2,3,4]));
